54debaafba2a3e56a98f2710b4cb53631b720971
Gianella A. Achetoni <135769239+Gianella-A-Achetoni@users.noreply.github.com>
2025-02-22 18:13:51 -0300

    update avances

--- MVP/src/page/chat/chat.tsx
+++ MVP/src/page/chat/chat.tsx
@@ -1,11 +1,25 @@
 const Chat = () => {
   return (
     <div className="w-100 vh-100 bg-dark d-flex justify-content-center align-items-center">
-      <div className="bg-secondary text-white p-4 rounded" style={{width: "90%", height:"80%"}}>
-        <div></div>
-        <div className="d-flex">
-          <textarea className="form-control mt-2 " style={{width:"90%", height:"35px", maxHeight:"80px"}} placeholder=""/> 
-          <button className="mt-2 ms-2" style={{width:"5%", height:"30px"}}> D </button>
+      <div className="bg-secondary text-white p-4 rounded position-relative" style={{ width: "90%", height: "80%" }}>
+        <div className="h-100 d-flex flex-column justify-content-between">
+          {/* Acalos mensajes */}
+          <div className="flex-grow-1"></div>
+
+          {/* Área de entrada de texto */}
+          <div className="d-flex align-items-start position-absolute bottom-0 p-3" style={{ width: "96%" }}>
+            <textarea 
+              className="form-control" 
+              style={{ height: "40px", maxHeight: "80px" }} 
+              placeholder="Escribe aqui..." 
+            />
+            <button 
+              className="ms-2 rounded border border-0 bg-dark text-white px-3" 
+              style={{ height: "40px" }}
+            >
+              Enviar
+            </button>
+          </div>
         </div>
       </div>
     </div>
