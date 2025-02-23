2d240d370519a7f8a8e6585fe50f3e27a34338be
Gianella A. Achetoni <135769239+Gianella-A-Achetoni@users.noreply.github.com>
2025-02-23 11:53:21 -0300

    update imag

--- MVP/src/page/select/select.tsx
+++ MVP/src/page/select/select.tsx
@@ -2,15 +2,15 @@
 const Select = () => {
   return (
     <div className="bg-black w-100 vh-100 d-flex flex-row justify-content-center align-items-center text-white">
-        <div className="p-2" style={{marginRight:"20%"}}>
-            <img src="" alt="" className="" style={{height: "250px", width: "250px", borderRadius: "50%"}}/>
-            <h2 className="">Equipo 1</h2>
-            <p className=""></p>
+        <div className="p-2" style={{marginRight:"20%",  maxWidth:"300px"}}>
+            <img src="https://github.com/aiweekend2025/FrontEnd/blob/Ginella_chat/MVP/src/assets/WhatsApp%20Image%202025-02-23%20at%209.32.58%20AM.jpeg" alt="" className="" style={{height: "250px", width: "250px", borderRadius: "50%"}}/>
+            <h2 className="">Vilma y Jean</h2>
+            <p className="">Vilma es una experta en y Jean es un exporto en , este equipo encontraras lo que necesitas todo lo que necesitas para tu campaña de marketing. </p>
         </div>
         <div className="ms-5 p-2">
             <img src="" alt="" className="" style={{height: "250px", width: "250px", borderRadius: "50%"}}/>
             <h2 className="">Otros</h2>
-            <p className="">Equipos multidisiplinarios </p>
+            <p className="">Equipos multidisiplinarios personalizados</p>
         </div>
     </div>
   )
